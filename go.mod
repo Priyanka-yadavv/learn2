987f1d8c8c16b4e9a64b0689cd7449e3934c6bda
Priyanka <josharsh@Harshs-MacBook-Pro.local>
2022-11-14 22:04:50 +0530

    initial version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Priyanka-yadavv/learn2
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,7 @@
+package learn2
+
+import "fmt"
+
+func GetGreeting(str string) string {
+	return fmt.Sprintf("Hey %v, good to see you", str)
+}
